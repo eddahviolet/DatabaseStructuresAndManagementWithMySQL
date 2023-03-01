919a799f031ce993e0d61f429f01131b8059b6b8
Eddah Violet <106580846+eddahviolet@users.noreply.github.com>
2023-03-01 14:43:32 +0300

    Add files via upload

--- Mission 1 Filtering data.sql
+++ Mission 1 Filtering data.sql
@@ -1,4 +1,12 @@
 use Lucky_Shrub;
+create table Orders (OrderID int not null primary key, ClientID varchar(10), ProductID varchar(10),
+Quantity int, Cost decimal(6,2));
+ 
+ insert into Orders (OrderID, ClientID, ProductID , Quantity, Cost) values (1, "Cl1", "P1", 10, 500), 
+ (2, "Cl2", "P2", 5, 100), (3, "Cl3", "P3", 20, 800), (4, "Cl4", "P4", 15, 150), (5, "Cl3", "P3", 10, 450),
+ (6, "Cl2", "P2", 5, 800), (7, "Cl1", "P4", 22, 1200), (8, "Cl3", "P1", 15, 150), (9, "Cl1", "P1", 10, 500),
+ (10, "Cl2", "P2", 5, 100);
+ 
 -- Task 1: Write a SQL statement to print all records of orders where the cost is $250 or less. 
 select * from Orders where Cost <= 250;
 
